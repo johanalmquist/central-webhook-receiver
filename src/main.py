6d89140f110c15cfa8ec43654374fe2ec110ca4f
Johan Almquist <johan.almquist@aranya.se>
2021-12-09 10:30:43 +0100

    send message to queue

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-import requests
+import json
+
+import pika
 from fastapi import Depends, FastAPI, Request
 
 from .core import models
 from .core.database import engine
 from .core.dependencies import validate_webhook
-from .core.logging import log
 from .core.routers import webhook
 
 app = FastAPI()
@@ -21,13 +22,9 @@ async def root():
 
 @app.post("/webhook")
 async def parse_comming(request: Request, is_okey: bool = Depends(validate_webhook)):
-    # rediect to action url
-    url = "http://notification:8001/slack"
     body = await request.json()
-    data = {"message": body["text"]}
-    try:
-        requests.post(url=url, json=data, timeout=120)
-        return True
-    except requests.exceptions.ReadTimeout:
-        log.warning(message="Did a timed out!")
-        return None
+    connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+    channel = connection.channel()
+    channel.queue_declare(queue="new-ap")
+    channel.basic_publish(exchange="", routing_key="new-ap", body=json.dumps(body))
+    connection.close()
